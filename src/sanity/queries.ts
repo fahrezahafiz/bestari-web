95cc6b186e0ed165fd88e158e69133bb19930da3
Fahreza Hafiz <fahreza_12@apps.ipb.ac.id>
2024-09-24 21:25:29 +0700

    iterate articles

--- src/sanity/queries.ts
+++ src/sanity/queries.ts
@@ -3,7 +3,7 @@ import { client } from "./client";
 import { Article, ArticlePreview } from "./types";
 
 export async function getArticles(): Promise<ArticlePreview[]> {
-    const ARTICLES_QUERY = defineQuery(`*[_type == "article" && defined(slug.current)]{
+    const ARTICLES_QUERY = defineQuery(`*[_type == "article" && defined(slug.current)][0...5]{
         _id, title, slug, abstract, coverImage {alt, "url": asset->url}
     }`);
     return await client.fetch(ARTICLES_QUERY) as ArticlePreview[];
@@ -13,5 +13,5 @@ export async function getArticleBySlug(slug: string): Promise<Article> {
     const ARTICLE_QUERY = defineQuery(`*[_type == "article" && slug.current == $slug][0]{
         _id, title, slug, abstract, content, coverImage {alt, "url": asset->url}
     }`);
-    return (await client.fetch(ARTICLE_QUERY, { slug }) as Article[])[0];
+    return await client.fetch(ARTICLE_QUERY, { slug }) as Article;
 }
