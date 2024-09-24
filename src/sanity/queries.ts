a95f712aa9dfd71b61f57796da4258e26e7f209b
Fahreza Hafiz <fahreza_12@apps.ipb.ac.id>
2024-09-24 13:32:15 +0700

    lfix queries

--- src/sanity/queries.ts
+++ src/sanity/queries.ts
@@ -1,9 +1,9 @@
 import { defineQuery } from "next-sanity";
 
-export const ARTICLES_QUERY = defineQuery(`*[_type == "article" && defined(slug.current)][0...5]{
-    _id, title, slug
+export const ARTICLES_QUERY = defineQuery(`*[_type == "article" && defined(slug.current)]{
+    _id, title, slug, abstract, coverImage
 }`);
 
-export const ARTICLE_QUERY = defineQuery(`*[_type == "post" && slug.current == $slug][0]{
-    title, content
+export const ARTICLE_QUERY = defineQuery(`*[_type == "article" && slug.current == $slug][0]{
+    _id, title, slug, abstract, content, coverImage
 }`);
