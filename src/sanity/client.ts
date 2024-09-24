2b0d60155e17215ce4eccc301d95b3c39d600123
Fahreza Hafiz <fahreza_12@apps.ipb.ac.id>
2024-09-24 13:21:20 +0700

    set useCdn to false

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -3,5 +3,5 @@ import {createClient} from 'next-sanity'
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
 });
