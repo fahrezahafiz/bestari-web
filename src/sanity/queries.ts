9ad47866c1ae7d2c2e04bbd7cf220157bb9fcc1b
Fahreza Hafiz <fahreza_12@apps.ipb.ac.id>
2024-09-24 20:42:44 +0700

    add types and queries

--- src/sanity/queries.ts
+++ src/sanity/queries.ts
@@ -1,9 +1,17 @@
 import { defineQuery } from "next-sanity";
+import { client } from "./client";
+import { Article, ArticlePreview } from "./types";
 
-export const ARTICLES_QUERY = defineQuery(`*[_type == "article" && defined(slug.current)]{
-    _id, title, slug, abstract, coverImage
-}`);
+export async function getArticles(): Promise<ArticlePreview[]> {
+    const ARTICLES_QUERY = defineQuery(`*[_type == "article" && defined(slug.current)]{
+        _id, title, slug, abstract, coverImage {alt, "url": asset->url}
+    }`);
+    return await client.fetch(ARTICLES_QUERY) as ArticlePreview[];
+}
 
-export const ARTICLE_QUERY = defineQuery(`*[_type == "article" && slug.current == $slug][0]{
-    _id, title, slug, abstract, content, coverImage
-}`);
+export async function getArticleBySlug(slug: string): Promise<Article> {
+    const ARTICLE_QUERY = defineQuery(`*[_type == "article" && slug.current == $slug][0]{
+        _id, title, slug, abstract, content, coverImage {alt, "url": asset->url}
+    }`);
+    return (await client.fetch(ARTICLE_QUERY, { slug }) as Article[])[0];
+}
