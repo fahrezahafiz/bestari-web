1b2c78361af8e6520ed172c846b1eab17d5afd20
Fahreza Hafiz <fahreza_12@apps.ipb.ac.id>
2024-10-03 02:14:14 +0700

    set useCdn to false

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -3,4 +3,7 @@ import {createClient} from 'next-sanity'
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: 'v2022-03-07',
+  perspective: 'published',
+  useCdn: false,
 });
